d5db0dcace0e72914e8d95a62c70321e6d74799b
ayushmaan <ayushmaanroy.12@gmail.com>
2022-12-16 13:26:56 +0530

    created server using express

---
+++ index.js
@@ -0,0 +1,18 @@
+//require express for setting up the server
+const express = require('express');
+//set up the Port Number
+const port = 6000;
+
+
+//using exprsss
+const app = express();
+
+
+//make the app listen on assigned port number
+app.listen(port,function(err,data){
+    if(err){
+     console.log(`error in runnig the server: ${err}`);
+    }
+    console.log(`server is running on port:${port}`)
+ 
+ })
